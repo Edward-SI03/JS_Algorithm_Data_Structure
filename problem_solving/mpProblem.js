92b4cb0ef09aee6d725e7c36fc9c4d5c67b49f01
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-12-18 22:18:11 +0900

    averagePair

--- problem_solving/mpProblem.js
+++ problem_solving/mpProblem.js
@@ -59,6 +59,26 @@ function areThereDuplicates() {
   return console.log(false);
 }
 
-areThereDuplicates(1, 2, 3); // false
-areThereDuplicates(1, 2, 2); // true
-areThereDuplicates("a", "b", "c", "c"); // true
+// areThereDuplicates(1, 2, 3); // false
+// areThereDuplicates(1, 2, 2); // true
+// areThereDuplicates("a", "b", "c", "c"); // true
+
+/* averagePair라는 함수를 작성합니다. 
+정렬된 정수 배열과 목표 평균이 주어진 경우, 쌍의 평균이 목표 평균과 같은 값의 쌍이 배열에 있는지 확인합니다. 
+평균 대상과 일치하는 쌍이 둘 이상 있을 수 있습니다. */
+function averagePair(arr, n) {
+  let j = arr.length - 1;
+
+  for (let i = 0; i <= j; i++) {
+    let average = (arr[i] + arr[j]) / 2;
+
+    if (average === n) return console.log(true);
+    if (average > n) j--;
+  }
+  return console.log(false);
+}
+
+// averagePair([1, 2, 3], 2.5); // true
+// averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+// averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
+// averagePair([], 4); // false
